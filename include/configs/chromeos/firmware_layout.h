0c9a99834112eb6016250995ce13a18ab94015da
Che-Liang Chiou <clchiou@chromium.org>
2011-03-16 13:00:07 +0800

    Update firmware layout
    Chrome OS firmware specification has renamed most firmware sections. This CL
    also reserve more spaces for VPD sections (from 4KB to 64KB).
    
    BUG=chrome-os-partner:2333
    TEST=MAKEALL successfully
    
    Review URL: http://codereview.chromium.org/6694022
    
    Change-Id: I2fcb861d3a6132937675de4c2a0fcaef36b4bac8

--- include/configs/chromeos/firmware_layout.h
+++ include/configs/chromeos/firmware_layout.h
@@ -1,57 +1,19 @@
 /*
- * Copyright 2011, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- * * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Alternatively, this software may be distributed under the terms of the
- * GNU General Public License ("GPL") version 2 as published by the Free
- * Software Foundation.
+ * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 #ifndef __FIRMWARE_LAYOUT_H__
 #define __FIRMWARE_LAYOUT_H__
 
-#define CONFIG_FIRMWARE_SIZE	0x00400000 /* 4 MB */
+/* TODO(clchiou): Below are old firmware layouts, will be removed soon */
 
 #define CONFIG_OFFSET_BOOTSTUB	0x00000000
-#define CONFIG_LENGTH_BOOTSTUB	0x00050000
+#define CONFIG_LENGTH_BOOTSTUB	0x000a0000
 
-#define CONFIG_OFFSET_RECOVERY	0x00050000
-#define CONFIG_LENGTH_RECOVERY	0x00100000
-
-#define CONFIG_OFFSET_FMAP	0x00150000
-#define CONFIG_LENGTH_FMAP	0x00010000
-
-#define CONFIG_OFFSET_GBB	0x00160000
-#define CONFIG_LENGTH_GBB	0x00040000
-
-#define CONFIG_OFFSET_ROVPD	0x001a0000
-#define CONFIG_LENGTH_ROVPD	0x00020000
+#define CONFIG_OFFSET_ROVPD	0x001f0000
+#define CONFIG_LENGTH_ROVPD	0x00010000
 
 #define CONFIG_OFFSET_FW_A_KEY	0x00200000
 #define CONFIG_LENGTH_FW_A_KEY	0x00010000
@@ -66,15 +28,90 @@
 #define CONFIG_LENGTH_FW_B_DATA	0x000d0000
 
 #define CONFIG_OFFSET_RWVPD	0x003c0000
-#define CONFIG_LENGTH_RWVPD	0x00001000
+#define CONFIG_LENGTH_RWVPD	0x00010000
 
 #define CONFIG_OFFSET_LOGVOL	0x003d0000
-#define CONFIG_LENGTH_LOGVOL	0x0002f000
+#define CONFIG_LENGTH_LOGVOL	0x00030000
+
+/* TODO(clchiou): Old firmware layout ends here */
+
+#define CONFIG_FIRMWARE_SIZE		0x00400000 /* 4 MB */
+
+/* -- Region: Read-only ----------------------------------------------------- */
+
+/* ---- Section: Read-only firmware ----------------------------------------- */
+
+#define CONFIG_OFFSET_RO_SECTION	0x00000000
+#define CONFIG_LENGTH_RO_SECTION	0x001f0000
+
+#define CONFIG_OFFSET_BOOT_STUB		0x00000000
+#define CONFIG_LENGTH_BOOT_STUB		0x000a0000
+
+#define CONFIG_OFFSET_RECOVERY		0x000a0000
+#define CONFIG_LENGTH_RECOVERY		0x00100000
+
+#define CONFIG_OFFSET_RO_DATA		0x001a0000
+#define CONFIG_LENGTH_RO_DATA		0x00010000
+
+#define CONFIG_OFFSET_FMAP		0x001a0000
+#define CONFIG_LENGTH_FMAP		0x00000400
+
+#define CONFIG_OFFSET_GBB		0x001b0000
+#define CONFIG_LENGTH_GBB		0x00040000
+
+/* ---- Section: Vital-product data (VPD) ----------------------------------- */
+
+#define CONFIG_OFFSET_RO_VPD		0x001f0000
+#define CONFIG_LENGTH_RO_VPD		0x00010000
+
+/* -- Region: Writable ------------------------------------------------------ */
+
+/* ---- Section: Rewritable slot A ------------------------------------------ */
+
+#define CONFIG_OFFSET_RW_SECTION_A	0x00200000
+#define CONFIG_LENGTH_RW_SECTION_A	0x000e0000
+
+#define CONFIG_OFFSET_VBLOCK_A		0x00200000
+#define CONFIG_LENGTH_VBLOCK_A		0x00010000
+
+#define CONFIG_OFFSET_FW_MAIN_A		0x00210000
+#define CONFIG_LENGTH_FW_MAIN_A		0x000d0000
+
+#define CONFIG_OFFSET_DATA_A		0x00200000
+#define CONFIG_LENGTH_DATA_A		0x00000000
+
+/* ---- Section: Rewritable slot B ------------------------------------------ */
+
+#define CONFIG_OFFSET_RW_SECTION_B	0x002e0000
+#define CONFIG_LENGTH_RW_SECTION_B	0x000e0000
+
+#define CONFIG_OFFSET_VBLOCK_B		0x002e0000
+#define CONFIG_LENGTH_VBLOCK_B		0x00010000
+
+#define CONFIG_OFFSET_FW_MAIN_B		0x002f0000
+#define CONFIG_LENGTH_FW_MAIN_B		0x000d0000
+
+#define CONFIG_OFFSET_DATA_B		0x002e0000
+#define CONFIG_LENGTH_DATA_B		0x00000000
+
+/* ---- Section: Rewritable VPD --------------------------------------------- */
+
+#define CONFIG_OFFSET_RW_VPD		0x003c0000
+#define CONFIG_LENGTH_RW_VPD		0x00010000
+
+/* ---- Section: Rewritable shared ------------------------------------------ */
+
+#define CONFIG_OFFSET_RW_SHARED		0x003d0000
+#define CONFIG_LENGTH_RW_SHARED		0x00030000
+
+#define CONFIG_OFFSET_DEV_CFG		0x003d0000
+#define CONFIG_LENGTH_DEV_CFG		0x00011000
+
+#define CONFIG_OFFSET_SHARED_DATA	0x003e1000
+#define CONFIG_LENGTH_SHARED_DATA	0x0001f000
 
 /* where are the meanings of these documented? Add a comment/link here */
-#define CONFIG_OFFSET_ENV	0x003ff000
-#define CONFIG_LENGTH_ENV	0x00001000
-
-
+#define CONFIG_OFFSET_ENV		0x003ff000
+#define CONFIG_LENGTH_ENV		0x00001000
 
 #endif /* __FIRMWARE_LAYOUT_H__ */
