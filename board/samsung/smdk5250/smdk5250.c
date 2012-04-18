95d56cee72534826c07e63a9105b79a521f76cad
Ajay Kumar <ajaykumar.rs@samsung.com>
2012-04-19 01:30:32 +0530

    exynos: add init calls for mshc for smdk5250 evt1
    Signed-off-by: Ajay Kumar <ajaykumar.rs@samsung.com>

--- board/samsung/smdk5250/smdk5250.c
+++ board/samsung/smdk5250/smdk5250.c
@@ -217,11 +217,13 @@ int checkboard(void)
 #ifdef CONFIG_GENERIC_MMC
 int board_mmc_init(bd_t *bis)
 {
+	exynos_pinmux_config(PERIPH_ID_SDMMC0, PINMUX_FLAG_8BIT_MODE);
 	exynos_pinmux_config(PERIPH_ID_SDMMC2, PINMUX_FLAG_NONE);
-	exynos_pinmux_config(PERIPH_ID_SDMMC4, PINMUX_FLAG_8BIT_MODE);
 #ifdef CONFIG_OF_CONTROL
-	s5p_mmc_init(gd->fdt_blob);
-	s5p_mshci_init(gd->fdt_blob);
+	setup_mshci_clock();
+	s5p_mshci_init(0, gd->fdt_blob);
+	s5p_mshci_init(2, gd->fdt_blob);
+
 #else
 	s5p_mmc_init(2, 4);
 	s5p_mshci_init(NULL);
