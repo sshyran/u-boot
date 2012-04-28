e288c2c05828b94deccb07a615b29f11981a96fc
Alim Akhtar <alim.akhtar@samsung.com>
2012-04-27 18:11:36 -0700

    exynos: dram: update new memory timings
    update new SDRAM timings parameters based on our memory test.
    
    BUG=none
    TEST=boot on daisy
    
    Change-Id: Ic05cc6d68c6b390a9efe9576b6dc75259e2644a1
    Signed-off-by: Alim Akhtar <alim.akhtar@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/21429
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Commit-Ready: Banajit Goswami <banajit.g@samsung.com>

--- arch/arm/cpu/armv7/exynos5/dmc_init_ddr3_evt1.c
+++ arch/arm/cpu/armv7/exynos5/dmc_init_ddr3_evt1.c
@@ -31,62 +31,12 @@
 #include "clock_init.h"
 #include "setup.h"
 
-#if 0
-static void reset_phy_ctrl(void)
-{
-	struct exynos5_clock *clk = (struct exynos5_clock *)EXYNOS5_CLOCK_BASE;
-
-	writel(LPDDR3PHY_CTRL_PHY_RESET_OFF, &clk->lpddr3phy_ctrl);
-	writel(LPDDR3PHY_CTRL_PHY_RESET, &clk->lpddr3phy_ctrl);
-}
-
-static void config_ctrl_dll_on(unsigned int state,
-			struct exynos5_phy_control *phy0_ctrl,
-			struct exynos5_phy_control *phy1_ctrl)
-{
-	unsigned int val, tmp;
-
-	val = readl(&phy0_ctrl->phy_con13);
-
-	/* Reading ctrl_lock_value[8:2] */
-	val &= (NR_DELAY_CELL_COARSE_LOCK_MASK <<
-		NR_DELAY_CELL_COARSE_LOCK_OFFSET);
-
-	/* Aligning 'val' to match 'ctrl_force' offset of PHY_CON12 */
-	val >>= CTRL_CLOCK_OFFSET;
-
-	/* Setting the PHY_CON12 register */
-	tmp = PHY_CON12_RESET_VAL;
-	CONFIG_CTRL_DLL_ON(tmp, state);
-
-	/* Writing 'val' in the 'ctrl_force' offset of PHY_CON12 */
-	tmp |= val;
-	writel(tmp, &phy0_ctrl->phy_con12);
-
-	val = readl(&phy1_ctrl->phy_con13);
-
-	/* Reading ctrl_lock_value[8:2] */
-	val &= (NR_DELAY_CELL_COARSE_LOCK_MASK <<
-		NR_DELAY_CELL_COARSE_LOCK_OFFSET);
-
-	/* Aligning 'val' to match 'ctrl_force' offset of PHY_CON12 */
-	val >>= CTRL_CLOCK_OFFSET;
-
-	/* Setting the PHY_CON12 register */
-	tmp = PHY_CON12_RESET_VAL;
-	CONFIG_CTRL_DLL_ON(tmp, state);
-
-	/* Writing 'val' in the 'ctrl_force' offset of PHY_CON12 */
-	tmp |= val;
-	writel(tmp, &phy1_ctrl->phy_con12);
-}
-#endif
-
 void mem_ctrl_init(void)
 {
 	struct exynos5_phy_control *phy0_ctrl, *phy1_ctrl;
 	struct exynos5_dmc *dmc;
 	struct exynos5_clock *clk = (struct exynos5_clock *)EXYNOS5_CLOCK_BASE;
+	unsigned int val;
 
 	phy0_ctrl = (struct exynos5_phy_control *)EXYNOS5_DMC_PHY0_BASE;
 	phy1_ctrl = (struct exynos5_phy_control *)EXYNOS5_DMC_PHY1_BASE;
@@ -106,23 +56,31 @@ void mem_ctrl_init(void)
 	writel(0x1, &clk->lpddr3phy_ctrl);
 	sdelay(1000);
 
-	writel(0x6db, &phy0_ctrl->phy_con39);
-	writel(0x6db, &phy1_ctrl->phy_con39);
+	writel(0xb6d, &phy0_ctrl->phy_con39);
+	writel(0xb6d, &phy1_ctrl->phy_con39);
 
 	writel(0x80b, &phy0_ctrl->phy_con42);
 	writel(0x80b, &phy1_ctrl->phy_con42);
 
-	writel(0x0e240304, &phy0_ctrl->phy_con16);
-	writel(0x0e240304, &phy1_ctrl->phy_con16);
+	writel(0xD2C0304, &phy0_ctrl->phy_con16);
+	writel(0xD2C0304, &phy1_ctrl->phy_con16);
 
-	writel(0x0e240304, &phy0_ctrl->phy_con16);
-	writel(0x0e240304, &phy1_ctrl->phy_con16);
+	writel(0xD2C0304, &phy0_ctrl->phy_con16);
+	writel(0xD2C0304, &phy1_ctrl->phy_con16);
 
-	writel(0x0e240306, &phy0_ctrl->phy_con16);
-	writel(0x0e240306, &phy1_ctrl->phy_con16);
+	writel(0xD2C0306, &phy0_ctrl->phy_con16);
+	writel(0xD2C0306, &phy1_ctrl->phy_con16);
 
-	writel(0x0e240304, &phy0_ctrl->phy_con16);
-	writel(0x0e240304, &phy1_ctrl->phy_con16);
+	do {
+		val = readl(0x10c00048);
+	} while (val & 0x1 != 1);
+
+	do {
+		val = readl(0x10c10048);
+	} while (val & 0x1 != 1);
+
+	writel(0xD2C0304, &phy0_ctrl->phy_con16);
+	writel(0xD2C0304, &phy1_ctrl->phy_con16);
 
 	writel(0xf, &phy0_ctrl->phy_con14);
 	writel(0xf, &phy1_ctrl->phy_con14);
@@ -158,7 +116,7 @@ void mem_ctrl_init(void)
 
 	writel((0x0FFF0000|(0x3<<12)), &dmc->concontrol);
 
-	writel(0x3, &dmc->ivcontrol);
+	writel(0x7, &dmc->ivcontrol);
 
 	writel(0x00001333, &dmc->memconfig0);
 
@@ -201,7 +159,7 @@ void mem_ctrl_init(void)
 	writel(0x00030000, &dmc->directcmd);
 	sdelay(100000);
 
-	writel((0x00010000|0x42), &dmc->directcmd);
+	writel((0x00010000|0x0), &dmc->directcmd);
 	sdelay(100000);
 
 	writel((0x00000000|0xD70), &dmc->directcmd);
@@ -219,7 +177,7 @@ void mem_ctrl_init(void)
 	writel(0x10030000, &dmc->directcmd);
 	sdelay(100000);
 
-	writel((0x10010000|0x42), &dmc->directcmd);
+	writel((0x10010000|0x0), &dmc->directcmd);
 	sdelay(100000);
 
 	writel((0x10000000|0xD70), &dmc->directcmd);
@@ -228,234 +186,49 @@ void mem_ctrl_init(void)
 	writel(0x1a000000, &dmc->directcmd);
 	sdelay(100000);
 
+	writel(0x17020a40, 0x10C00000);
+	writel(0x17020a40, 0x10C10000);
+	writel(0x17024a40, 0x10C00000);
+	writel(0x17024a40, 0x10C10000);
+	writel(0x10044, 0x10C00008);
+	writel(0x10044, 0x10C10008);
+	writel(0x17026a40, 0x10C00000);
+	writel(0x17026a40, 0x10C10000);
+	writel(0x10101a50, 0x10C00030);
+	writel(0x10101a50, 0x10C10030);
+	writel(0x1010044, 0x10C00008);
+	writel(0x1010044, 0x10C10008);
+	writel(0x17026b40, 0x10C00000);
+	writel(0x17026b40, 0x10C10000);
+	writel(0x9010100, 0x10C00004);
+	writel(0x9010100, 0x10C10004);
+	writel(0x00000001, 0x10DD00f8);
+	sdelay(2000);
+
+	do {
+		val = readl(0x10DD0040);
+	} while (val & 0xC000 != 0xC000);
+
+	writel(0x00000000, 0x10DD00f8);
+	writel(0x00000000, 0x10C00038);
+	writel(0x00000000, 0x10C10038);
+	writel(0x10101a70, 0x10C00030);
+	writel(0x10101a70, 0x10C10030);
+	sdelay(1000);
+
+	writel(0xe0000008, 0x10DD0018);
+	writel(0xe0000000, 0x10DD0018);
+	writel(0x01000000, 0x10DD0010);
+	sdelay(1000);
+
+	writel(0x01100000, 0x10DD0010);
+	sdelay(1000);
+
+	writel(0x11000000, 0x10DD0010);
+	sdelay(1000);
+
+	writel(0x11100000, 0x10DD0010);
+
 	writel(0x00302620, &dmc->memcontrol);
 	writel((0x0FFF0020|(0x3<<12)), &dmc->concontrol);
-
 }
-#if 0
-/*==========old one==========-*/
-
-	writel(, &dmc->directcmd);
-	sdelay(100000);
-
-	writel(0x1fff2100, &dmc->concontrol);
-
-	writel(0x80000000, &clk->lpddr3phy_ctrl);
-
-	writel(0x1, &phy0_ctrl->phy_con24);
-	writel(0x1, &phy1_ctrl->phy_con24);
-
-	writel(0xf, &phy0_ctrl->phy_con14);
-	writel(0xf, &phy1_ctrl->phy_con14);
-
-	writel(0x7107f, &phy0_ctrl->phy_con26);
-	writel(0x7107f, &phy1_ctrl->phy_con26);
-
-	writel(0x17021A00, &phy0_ctrl->phy_con0);
-	writel(0x8080304, &phy0_ctrl->phy_con16);
-
-	writel(0x17021A00, &phy1_ctrl->phy_con0);
-	writel(0x8080304, &phy1_ctrl->phy_con16);
-
-	writel(0xe0C0304, &phy0_ctrl->phy_con16);
-	writel(0xe0C0306, &phy0_ctrl->phy_con16);
-	do {
-		val = readl(&phy0_ctrl->phy_con17);
-	} while ((val & 0x1) != 1);
-	writel(0xe080304, &phy0_ctrl->phy_con16);
-
-	writel(0xe0C0304, &phy1_ctrl->phy_con16);
-	writel(0xe0C0306, &phy1_ctrl->phy_con16);
-	do {
-		val = readl(&phy1_ctrl->phy_con17);
-	} while ((val & 0x1) != 1);
-	writel(0xe080304, &phy1_ctrl->phy_con16);
-
-	writel(0x0fff2100, &dmc->concontrol);
-	writel(0x1fff2100, &dmc->concontrol);
-	sdelay(10000);
-	writel(0x0fff2100, &dmc->concontrol);
-
-	writel(0x312700, &dmc->memcontrol);
-	writel(0x4007c0, &dmc->membaseconfig0);
-	writel(0x8007c0, &dmc->membaseconfig1);
-	writel(0x1323, &dmc->memconfig0);
-	writel(0x1323, &dmc->memconfig0);
-	writel(0x7, &dmc->ivcontrol);
-	writel(0x5d, &dmc->timingref);
-	writel(0x34498692, &dmc->timingrow);
-	writel(0x3631D60C, &dmc->timingdata);
-	writel(0x50380336, &dmc->timingpower);
-
-	writel(0xfff, &dmc->qoscontrol0);
-	writel(0xfff, &dmc->qoscontrol1);
-	writel(0xfff, &dmc->qoscontrol2);
-	writel(0xfff, &dmc->qoscontrol3);
-	writel(0xfff, &dmc->qoscontrol4);
-	writel(0xfff, &dmc->qoscontrol5);
-	writel(0xfff, &dmc->qoscontrol6);
-	writel(0xfff, &dmc->qoscontrol7);
-	writel(0xfff, &dmc->qoscontrol8);
-	writel(0xfff, &dmc->qoscontrol9);
-	writel(0xfff, &dmc->qoscontrol10);
-	writel(0xfff, &dmc->qoscontrol11);
-	writel(0xfff, &dmc->qoscontrol12);
-	writel(0xfff, &dmc->qoscontrol13);
-	writel(0xfff, &dmc->qoscontrol14);
-	writel(0x0, &dmc->qoscontrol15);
-
-	writel(0x7f7f7f7f, &phy0_ctrl->phy_con4);
-	writel(0x7f7f7f7f, &phy1_ctrl->phy_con4);
-
-	writel(0x7f7f7f7f, &phy0_ctrl->phy_con6);
-	writel(0x7f7f7f7f, &phy1_ctrl->phy_con6);
-
-	writel(0x7f, &phy0_ctrl->phy_con10);
-	writel(0x7f, &phy1_ctrl->phy_con10);
-
-	writel(0x10107f70, &phy0_ctrl->phy_con12);
-	writel(0x10107f70, &phy1_ctrl->phy_con12);
-
-	writel(0x10107f50, &phy0_ctrl->phy_con12);
-	writel(0x10107f50, &phy1_ctrl->phy_con12);
-
-	writel(0x8, &dmc->phycontrol0);
-	writel(0x0, &dmc->phycontrol0);
-
-	/* Channel0 - Chip0 */
-	writel(0x7000000, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x71c00, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10bfc, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x50c, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x868, &dmc->directcmd);
-	sdelay(1000);
-	writel(0xc04, &dmc->directcmd);
-	sdelay(1000);
-
-	/* Channel0 - Chip1 */
-	writel(0x7100000, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x171c00, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x110bfc, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10050c, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x100868, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x100c04, &dmc->directcmd);
-	sdelay(1000);
-
-	/* Channel1 - Chip0 */
-	writel(0x17000000, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10071c00, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10010bfc, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x1000050c, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10000868, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10000c04, &dmc->directcmd);
-	sdelay(1000);
-
-	/* Channel1 - Chip0 */
-	writel(0x17100000, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10171c00, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10110bfc, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x1010050c, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10100868, &dmc->directcmd);
-	sdelay(1000);
-	writel(0x10100c04, &dmc->directcmd);
-	sdelay(1000);
-
-	writel(0x0, &clk->src_cdrex);
-	do {
-		val = readl(&clk->mux_stat_cdrex);
-	} while(val != 0x00211111);
-
-	writel(0x01000051, &clk->div_cdrex);
-	do {
-		val = readl(&clk->div_stat_cdrex);
-	} while(val != 0);
-
-	clrbits_le32(&clk->src_core1, MUX_MPLL_SEL_MASK);
-	do {
-		val = readl(&clk->mux_stat_core1);
-	} while(val != MUX_MPLL_SEL_MASK);
-
-	writel(MPLL_CON1_VAL, &clk->mpll_con1);
-	writel(MPLL_CON0_VAL, &clk->mpll_con0);
-	sdelay(0x10000);
-
-	clrbits_le32(&clk->src_core1, MUX_MPLL_SEL_MASK);
-	do {
-		val = readl(&clk->mux_stat_core1);
-	} while(val != MPLL_SEL_MOUT_MPLLFOUT);
-
-	writel(0x08080808, &phy0_ctrl->phy_con4);
-	writel(0x08080808, &phy1_ctrl->phy_con4);
-
-	writel(0x08080808, &phy0_ctrl->phy_con6);
-	writel(0x08080808, &phy1_ctrl->phy_con6);
-
-	writel(0x08, &phy0_ctrl->phy_con10);
-	writel(0x08, &phy1_ctrl->phy_con10);
-
-	writel(0x10107f70, &phy0_ctrl->phy_con12);
-	sdelay(1000);
-	writel(0x10107f30, &phy0_ctrl->phy_con12);
-	writel(0x10107f70, &phy0_ctrl->phy_con12);
-	sdelay(1000);
-
-	writel(0x10107f70, &phy1_ctrl->phy_con12);
-	sdelay(1000);
-	writel(0x10107f30, &phy1_ctrl->phy_con12);
-	writel(0x10107f70, &phy1_ctrl->phy_con12);
-	sdelay(1000);
-
-	writel(0x1fff2100, &dmc->concontrol);
-	do {
-		val = readl(&dmc->phystatus);
-	} while ((val & 0xc) != 0xc);
-	writel(0x0fff2100, &dmc->concontrol);
-
-	writel(0x8, &dmc->phycontrol0);
-	writel(0x0, &dmc->phycontrol0);
-
-	writel(0x17021A00, &phy0_ctrl->phy_con0);
-	writel(0xe080304, &phy0_ctrl->phy_con16);
-
-	writel(0x17021A00, &phy1_ctrl->phy_con0);
-	writel(0xe080304, &phy1_ctrl->phy_con16);
-
-	writel(0x1000000, &dmc->directcmd);
-	writel(0x1100000, &dmc->directcmd);
-	writel(0x11000000, &dmc->directcmd);
-	writel(0x11100000, &dmc->directcmd);
-
-	writel(0x312710, &dmc->memcontrol);
-	writel(0xff000000, &dmc->prechconfig);
-	writel(0xffff00ff, &dmc->pwrdnconfig);
-	val = readl(&dmc->memcontrol);
-	val |= 0x20;
-	writel(val, &dmc->memcontrol);
-	writel(0xffff00ff, &dmc->pwrdnconfig);
-	val = readl(&dmc->memcontrol);
-	val |= 0x2;
-	writel(val, &dmc->memcontrol);
-
-	setbits_le32(&dmc->memcontrol, 0x1);
-
-	writel(0xffff2108, &dmc->concontrol);
-	writel(0x1fff2128, &dmc->concontrol);
-#endif
