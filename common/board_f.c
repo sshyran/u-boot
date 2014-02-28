archlinuxarm/u-boot-chromebook2

exynos: spl: gen: enable dcache in SPL
When booting from SPI, SD or eMMC, enable dcache early in SPL. This
would help boot time once dcache state is handed over from SPL to
u-boot.

The size of DRAM covered by dcache is limited to 2GB, this should be
enhanced to cover actual amount of installed DRAM.

Use 16K in SRAM to store the TLB. In reserve_mmu() reuse the existing
TLB if mmu happens to be enabled.

BRANCH=none
BUG=chrome-os-partner:19927
TEST=manual
  . built and booted images using SD card, SPI flash and USB A-A.
    U-boot is still reinitializing the dcache in the CPU startup code,
    this is why performance improvements are not that significant.

Change-Id: I82e45a88ce6bf846c89a209469b3d7406b6fccaa
Signed-off-by: Simon Glass <sjg@chromium.org>
Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/168512

common/board_f.c
@@ -469,14 +469,20 @@ static int reserve_round_4k(void)
 		defined(CONFIG_ARM)
 static int reserve_mmu(void)
 {
-	/* reserve TLB table */
 	gd->arch.tlb_size = 4096 * 4;
-	gd->dest_addr -= gd->arch.tlb_size;
 
-	/* round down to next 64 kB limit */
-	gd->dest_addr &= ~(0x10000 - 1);
+	if (dcache_status()) {
+		/* If the cache is already on, use the existing MMU address */
+		gd->arch.tlb_addr = get_ttbr();
+	} else {
+		/* reserve TLB table */
+		gd->dest_addr -= gd->arch.tlb_size;
 
-	gd->arch.tlb_addr = gd->dest_addr;
+		/* round down to next 64 kB limit */
+		gd->dest_addr &= ~(0x10000 - 1);
+
+		gd->arch.tlb_addr = gd->dest_addr;
+	}
 	debug("TLB table from %08lx to %08lx\n", gd->arch.tlb_addr,
 	      gd->arch.tlb_addr + gd->arch.tlb_size);
 	return 0;

sshyran/u-boot

Conflicts in arch/arm/cpu/armv7/exynos/spl_boot.c, common/board_f.c,
and include/configs/exynos5-dt.h were resolved manually.
An important change is moving the spl_use_dcache() check into
copy_uboot_to_ram() as boot mode is adjusted inside the copy function.
(cherry picked from commit 7414d8ba023cee429f86b10524f8e8189548aac0)
Reviewed-on: https://chromium-review.googlesource.com/188108

common/board_f.c
@@ -466,14 +466,20 @@ static int reserve_round_4k(void)
 		defined(CONFIG_ARM)
 static int reserve_mmu(void)
 {
-	/* reserve TLB table */
 	gd->arch.tlb_size = 4096 * 4;
-	gd->dest_addr -= gd->arch.tlb_size;
 
-	/* round down to next 16 kB limit */
-	gd->dest_addr &= ~(0x4000 - 1);
+	if (dcache_status()) {
+		/* If the cache is already on, use the existing MMU address */
+		gd->arch.tlb_addr = get_ttbr();
+	} else {
+		/* reserve TLB table */
+		gd->dest_addr -= gd->arch.tlb_size;
 
-	gd->arch.tlb_addr = gd->dest_addr;
+		/* round down to next 16 kB limit */
+		gd->dest_addr &= ~(0x4000 - 1);
+
+		gd->arch.tlb_addr = gd->dest_addr;
+	}
 	debug("TLB table from %08lx to %08lx\n", gd->arch.tlb_addr,
 	      gd->arch.tlb_addr + gd->arch.tlb_size);
 	return 0;