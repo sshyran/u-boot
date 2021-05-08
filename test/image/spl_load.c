641ddc37dd9fbf7ab3ea9855d4d528e31ade7b2c
Simon Glass <sjg@chromium.org>
2021-05-08 16:49:03 -0600

    WIP: To be upstreamed
    This commit includes code not yet upstreamed to mainline.
    
    Change-Id: Ia69790585b4676ffadbe4fb56d5691c448addbb9

--- test/image/spl_load.c
+++ test/image/spl_load.c
@@ -69,10 +69,7 @@ static int spl_test_load(struct unit_test_state *uts)
 	load.bl_len = 512;
 	load.read = read_fit_image;
 
-	cur_prefix = spl_phase_prefix(spl_phase());
-	next_prefix = spl_phase_prefix(spl_next_phase());
-	ret = os_find_u_boot(fname, sizeof(fname), true, cur_prefix,
-			     next_prefix);
+	ret = sandbox_find_next_phase(fname, sizeof(fname), true);
 	if (ret) {
 		printf("(%s not found, error %d)\n", fname, ret);
 		return ret;
