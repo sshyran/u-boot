d2b588e96f99bee3b3986489cb4fecf3c0f3549c
Paweł Anikiel <pan@semihalf.com>
2022-06-17 12:47:21 +0200

    UPSTREAM: misc: atsha204a: Increase wake delay by tWHI
    From the ATSHA204A datasheet (document DS40002025A):
    
    Wake: If SDA is held low for a period greater than tWLO, the device
    exits low-power mode and, after a delay of tWHI, is ready to receive
    I2C commands.
    
    tWHI value can be found in table 7-2.
    
    Signed-off-by: Paweł Anikiel <pan@semihalf.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>
    (cherry picked from commit 73d88cf9719e505eedb389d117704f806e4747d1)
    Change-Id: I3558cb29e636877fa37edd66b8fc5ae1ac39fec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/u-boot/+/3673310
    Tested-by: Paweł Anikiel <panikiel@google.com>
    Reviewed-by: Alexandru Stan <amstan@chromium.org>
    Commit-Queue: Alexandru Stan <amstan@chromium.org>

--- drivers/misc/atsha204a-i2c.c
+++ drivers/misc/atsha204a-i2c.c
@@ -20,7 +20,8 @@
 #include <linux/delay.h>
 #include <u-boot/crc.h>
 
-#define ATSHA204A_TWLO			60
+#define ATSHA204A_TWLO_US		60
+#define ATSHA204A_TWHI_US		2500
 #define ATSHA204A_TRANSACTION_TIMEOUT	100000
 #define ATSHA204A_TRANSACTION_RETRY	5
 #define ATSHA204A_EXECTIME		5000
@@ -225,7 +226,7 @@ int atsha204a_wakeup(struct udevice *dev)
 			continue;
 		}
 
-		udelay(ATSHA204A_TWLO);
+		udelay(ATSHA204A_TWLO_US + ATSHA204A_TWHI_US);
 
 		res = atsha204a_recv_resp(dev, &resp);
 		if (res) {
